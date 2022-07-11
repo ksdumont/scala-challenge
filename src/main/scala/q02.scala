4a39a5ddb7bd9ab6a3e5f40b035113ce9ed08e42
Keith <keith.dumont@hginsights.com>
2022-07-11 11:22:15 -0700

    fixed .gitignore

--- src/main/scala/q02.scala
+++ src/main/scala/q02.scala
@@ -1,12 +1,12 @@
 object q02 {
   def main(args: Array[String]): Unit = {
-    // get length of args
+    // get length of data
     val length: Int = args.length
-
-    // check to make sure there are at least two numbers
+    // check to make sure there are at least two cities
     if (length < 2) {
       println("Please provide at least two cities in the arguments.")
-      return
+    } else {
+
     }
 
   }
